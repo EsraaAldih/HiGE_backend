c2f75ac0731f78e8c666f880f0bd45a34a1b70b3
halahamde <36208722+halahamde@users.noreply.github.com>
2022-02-21 23:28:03 +0200

    Update urls -Hala

--- app/api/urls.py
+++ app/api/urls.py
@@ -12,13 +12,7 @@ router.register(r'yogaplans', YogaPlanViewSet)
 router.register(r'comments', CommentViewSet)
 router.register(r'commentreports', ReportCommentViewSet)
 router.register(r'workoutexersices', WorkoutExViewSet)
-<<<<<<< HEAD
-
-
-=======
-router.register(r'weight', weightViewSet)
 router.register(r'water', WaterTrackerViewSet)
->>>>>>> 1b9029491068e3fd5b65a63b0c5f2daeda3cd89e
 
 urlpatterns = [
 
@@ -28,8 +22,5 @@ urlpatterns = [
     path('workoutfavplan/',getTraineeFavWorkoutPlan .as_view()),
     path('addYogaPlan/',addYogaPlan.as_view()),
     path('addWorkoutPlan/',addWorkoutPlan.as_view()),
-<<<<<<< HEAD
     path('TraineeCurrentWeight/',TraineeCurrentWeight.as_view()),
-=======
->>>>>>> 1b9029491068e3fd5b65a63b0c5f2daeda3cd89e
 ]
