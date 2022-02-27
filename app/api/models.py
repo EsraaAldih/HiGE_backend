18e5f1dd6da0abb7fc227cf066534b4d33423a70
Hanan Mohamed <7ananm7mddd@gmail.com>
2022-02-27 18:09:20 +0200

    Added show trainers clients class: Hanan

--- app/api/models.py
+++ app/api/models.py
@@ -191,6 +191,6 @@ class WeightTrackerHistory(models.Model):
 class WaterTrackerHistory(models.Model):
     dailyAmount = models.FloatField(default=0)
     traineeID = models.ForeignKey(Trainee, on_delete=models.SET_NULL, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
