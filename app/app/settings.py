e2ebd31ffd3d14eaaeef2065fd7cad63dcd9eb8f
EsraaAldih <esraa.aldih123@gmail.com>
2022-03-01 16:55:55 +0200

    edit settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'virgin2',
+        'NAME': 'virgin',
         'USER': 'djangos',
         'PASSWORD': 'django',
         'HOST': 'localhost',
